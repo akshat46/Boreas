93c0f7b392b81d6f0d7d157d7d54dab2c9b07d42
Vishnu Narayana <vishtheshnu@gmail.com>
2020-03-02 09:47:33 -0800

    Began refactoring code to allow for use of Google Nearby Connect beyond local group chat, and Google Nearby connecting now follows a timer rather than manual control for advertising and discovering.

---
+++ app/src/main/java/com/sjsu/boreas/database/User.java
@@ -0,0 +1,38 @@
+package com.sjsu.boreas.database;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
+public class User {
+
+    public User(String uid, String name, double latitude, double longitude, boolean isMe){
+        this.uid = uid;
+        this.name = name;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.isMe = isMe;
+    }
+
+    @NonNull
+    @PrimaryKey
+    public String uid;
+
+    @ColumnInfo(name = "me")
+    public boolean isMe;
+
+    @ColumnInfo(name = "name")
+    public String name;
+
+    @ColumnInfo(name = "latitude")
+    public double latitude;
+
+    @ColumnInfo(name = "longitude")
+    public double longitude;
+
+    public String toString(){
+        return name+": "+uid+"\n"+latitude + " , " + longitude+"\n" + (isMe ? "IS" : "NOT") + " me";
+    }
+}
---
+++ app/src/main/java/com/sjsu/boreas/messaging/ChatMessageAdapter.java
@@ -0,0 +1,79 @@
+package com.sjsu.boreas.messaging;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.drawable.GradientDrawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.sjsu.boreas.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChatMessageAdapter extends BaseAdapter {
+
+    List<Message> messages = new ArrayList<>();
+    private Context context;
+
+    public ChatMessageAdapter(Context context){
+        this.context = context;
+    }
+
+    public void addMessage(Message message){
+        messages.add(message);
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getCount() {
+        return messages.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return messages.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        MessageViewHolder holder = new MessageViewHolder();
+        LayoutInflater messageInflater = (LayoutInflater) context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+        Message message = messages.get(i);
+
+        if(message.getIsSender()){
+            view = messageInflater.inflate(R.layout.message_text_sent, null);
+            holder.messageBody = (TextView) view.findViewById(R.id.message_body);
+            view.setTag(holder);
+            holder.messageBody.setText(message.getText());
+        }else{
+            view = messageInflater.inflate(R.layout.message_text_received, null);
+            holder.name = (TextView) view.findViewById(R.id.name);
+            holder.messageBody = (TextView) view.findViewById(R.id.message_body);
+            view.setTag(holder);
+
+            holder.name.setText(message.getSender());
+            holder.messageBody.setText(message.getText());
+        }
+
+        return view;
+    }
+
+    class MessageViewHolder{
+        public TextView name;
+        public TextView messageBody;
+    }
+
+}
+
