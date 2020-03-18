d7685e9e474c19b0845315cd4b8c032713082588
maninderjits <sukhisembhy@hotmail.com>
2020-03-17 18:40:43 -0700

    Integrated firebase database

---
+++ app/src/main/java/com/sjsu/boreas/Firebase/RegisterOnFirebase.java
@@ -0,0 +1,38 @@
+package com.sjsu.boreas.Firebase;
+
+import android.util.Log;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.sjsu.boreas.database.User;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RegisterOnFirebase {
+
+    private static String TAG = "BOREAS";
+    private static String SUB_TAG = "---RegisterOnFirebase ";
+
+    private DatabaseReference database_ref;
+
+    public RegisterOnFirebase(){
+        Log.e(TAG, SUB_TAG+"RegisterOnFirebase");
+        database_ref = FirebaseDatabase.getInstance().getReference();
+    }
+
+    public void RegisterUserOnFirebase(User user){
+        Log.e(TAG, SUB_TAG+"RegisterUserOnFirebase");
+
+        Map<String, Object> new_user = user.toMap();
+
+        Map<String, Object> firebase_child_update = new HashMap<>();
+
+        //We are putting this data under the users branch of the firebase database
+        firebase_child_update.put("/users/" + user.getUid(), new_user);
+
+        //Do the actual writing of the data onto firebase
+        database_ref.updateChildren(firebase_child_update);
+    }
+
+}
