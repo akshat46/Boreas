307b12467ce492b7200676a864a4aeeb4e43e56d
kalyanponduri <kalyanponduri@gmail.com>
2020-03-31 08:06:19 -0700

    updated chat new activity

--- app/src/main/java/com/sjsu/boreas/ChatViewRelatedStuff/OneOnOneChatViewAdapter.java
+++ app/src/main/java/com/sjsu/boreas/ChatViewRelatedStuff/OneOnOneChatViewAdapter.java
@@ -1,14 +1,28 @@
 package com.sjsu.boreas.ChatViewRelatedStuff;
 
+import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
+import androidx.fragment.app.FragmentActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.sjsu.boreas.ChatActivity2;
+import com.sjsu.boreas.ChatNewActivity;
 import com.sjsu.boreas.R;
+import com.sjsu.boreas.ViewFragments.OneOnOneFragment;
 
 public class OneOnOneChatViewAdapter extends RecyclerView.Adapter<OneOnOneChatViewAdapter.MyViewHolder> {
+
+private Context activity;
+    public OneOnOneChatViewAdapter(Context activity) {
+        this.activity = activity;
+    }
+
     // Provide a reference to the views for each data item
     // Complex data items may need more than one view per item, and
     // you provide access to all the views for a data item in a view holder
@@ -29,6 +43,13 @@ public class OneOnOneChatViewAdapter extends RecyclerView.Adapter<OneOnOneChatVi
         // create a new view
         RelativeLayout v = (RelativeLayout) LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.item_chat, parent, false);
+        v.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(activity, ChatNewActivity.class);
+                activity.startActivity(intent);
+            }
+        });
         MyViewHolder vh = new MyViewHolder(v);
         return vh;
     }
