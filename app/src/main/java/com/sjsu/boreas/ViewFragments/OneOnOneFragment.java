9a8523293c35615b09df5a386383f8ae14c7e105
Maninderjit <maninderjit5embhy@hotmail.com>
2020-04-01 17:32:21 -0700

    List is showing up (but currently its all the users)

--- app/src/main/java/com/sjsu/boreas/ViewFragments/OneOnOneFragment.java
+++ app/src/main/java/com/sjsu/boreas/ViewFragments/OneOnOneFragment.java
@@ -1,6 +1,7 @@
 package com.sjsu.boreas.ViewFragments;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,14 +9,27 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.database.Query;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.firebase.ui.database.SnapshotParser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.sjsu.boreas.ChatViewRelatedStuff.OneOnOneChatViewAdapter;
+import com.sjsu.boreas.Firebase.GetFirebaseContacts;
 import com.sjsu.boreas.LandingPage;
 import com.sjsu.boreas.R;
+import com.sjsu.boreas.ViewHolder.UsersViewHolder;
+import com.sjsu.boreas.database.User;
 import com.sjsu.boreas.messaging.ChatActivity;
 import com.sjsu.boreas.messaging.ChatMessageAdapter;
 
@@ -23,7 +37,7 @@ public class OneOnOneFragment extends Fragment {
     public static final String EXTRA_TAB_NAME = "tab_name";
     private String mTabName;
     private RecyclerView recyclerView;
-    private RecyclerView.Adapter mAdapter;
+    private FirebaseRecyclerAdapter<User, UsersViewHolder> mAdapter;
     private RecyclerView.LayoutManager layoutManager;
     private LandingPage mParent;
     private View rootView;
@@ -76,11 +90,74 @@ public class OneOnOneFragment extends Fragment {
     }
 
     private void initUI() {
+        Log.e(TAG, SUB_TAG+"initUI");
         recyclerView = (RecyclerView) rootView.findViewById(R.id.my_recycler_view);
         recyclerView.setHasFixedSize(true);
         layoutManager = new LinearLayoutManager(mParent);
         recyclerView.setLayoutManager(layoutManager);
-        mAdapter = new OneOnOneChatViewAdapter();
+
+        initializeFirebaseAdapter();
+
         recyclerView.setAdapter(mAdapter);
     }
+
+    public void initializeFirebaseAdapter(){
+        Log.e(TAG, SUB_TAG+"InitializeFirebase adapter");
+        Query query = FirebaseDatabase.getInstance().getReference().child("users");
+
+        FirebaseRecyclerOptions<User> options = new FirebaseRecyclerOptions.Builder<User>()
+                .setQuery(query, new SnapshotParser<User>() {
+                    @NonNull
+                    @Override
+                    public User parseSnapshot(@NonNull DataSnapshot snapshot) {
+                        Log.e(TAG, SUB_TAG+"Parse snapshot");
+                        return new User(snapshot.child("uid").getValue().toString(),
+                                snapshot.child("name").getValue().toString(),
+                                Double.parseDouble(snapshot.child("latitude").getValue().toString()),
+                                Double.parseDouble(snapshot.child("longitude").getValue().toString()),
+                                false);
+                    }
+                })
+                .build();
+
+        mAdapter = new FirebaseRecyclerAdapter<User, UsersViewHolder>(options) {
+            @NonNull
+            @Override
+            public UsersViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                Log.e(TAG, SUB_TAG+"onCreateViewHolder");
+                View view = LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.item_chat, parent, false);
+
+                return new UsersViewHolder(view);
+            }
+
+            @Override
+            protected void onBindViewHolder(@NonNull UsersViewHolder holder, int position, @NonNull User model) {
+                Log.e(TAG, SUB_TAG+"onBindViewHolder");
+                holder.bindToListItemView(model);
+
+                holder.itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // Launch PostDetailActivity
+                        Intent intent = new Intent(getActivity(), ChatActivity.class);
+                        startActivity(intent);
+                    }
+                });
+            }
+        };
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAdapter.startListening();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mAdapter.stopListening();
+    }
+
 }
